53c4ea1359b2a9bf7c0658525a1cbf8e132fdce3
sloanlacey <sloanlacey89@gmail.com>
2022-06-07 17:39:05 -0700

    Setup some basic file sctructure, install fonts.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div>Hello Mother of Dogs</div>
+      <h1 className="bebas f-headline tc">Mother of Dogs</h1>
+      <h2 className="f2 ml5 barlow">Our Pack</h2>
+      <p className="catamaran f4 w-50 ml5">
+        Doggies are my favorite. Doggo ipsum heckin length boy sub woofer. I am
+        bekom fat shoober doggorino vvv adorable doggo borking doggo you are
+        doin me a concern, heck most angery pupper I have ever seen long doggo
+        sub woofer. Sub woofer doggo very good spot clouds doing me a frighten
+        stop it fren, heckin good boys and girls you are doing me the shock
+        super chub lotsa pats. Woofer smol he made many woofs boof doing me a
+        frighten mlem ruff, very jealous pupper boofers doggo yapper I am bekom
+        fat.
+      </p>
     </>
   );
 }
